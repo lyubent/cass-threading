fd021055760f416922e4d2111a5862d25fcc9dbb
lyubent <lyuben@mail.com>
2018-12-16 02:37:35 +0000

    Added yaml settings for benchmark type and C* nodes.

--- src/main/java/io/github/lyubent/MultiSessionBenchmark.java
+++ src/main/java/io/github/lyubent/MultiSessionBenchmark.java
@@ -10,25 +10,18 @@ public class MultiSessionBenchmark {
 
     // 480 for shared obj.
     public static void main(String[] args)  {
-        if (args.length < 2)
-            throw new ConfigException(CommonMsgs.tooFewArgs());
-
-        String[] contactPoints = new String[args.length-1];
-        for (int i=1; i<args.length; i++)
-            contactPoints[i-1] = args[i].trim();
-
 
         final CassthreadingConfig conf = CassthreadingConfig.getInstance();
 
-        // todo - needs better error handling
-        // todo - this 'func' doesn't belong in main.
-        String arg1 = args[0];
-        if (arg1.trim().toLowerCase().equals("thread")) {
-            new MultiThread(contactPoints, conf.getParallelism()).runBenchmark();
-        } else if (arg1.trim().toLowerCase().equals("session")) {
-            new MultiSession(contactPoints, conf.getParallelism()).runBenchmark();
-        } else {
-            throw new ConfigException(CommonMsgs.invalidArg1(arg1));
+        switch (conf.getBenchmark().trim()) {
+            case "thread":
+                new MultiThread(conf.getNodes(), conf.getParallelism()).runBenchmark();
+                break;
+            case "session":
+                new MultiSession(conf.getNodes(), conf.getParallelism()).runBenchmark();
+                break;
+            default:
+                throw new ConfigException(CommonMsgs.missconfiguredYaml("benchmark", "'thread' or 'session'"));
         }
     }
 }
--- src/main/java/io/github/lyubent/config/CassthreadingConfig.java
+++ src/main/java/io/github/lyubent/config/CassthreadingConfig.java
@@ -15,6 +15,25 @@ public class CassthreadingConfig {
     }
 
     private Integer parallelism;
+    private String[] nodes;
+
+    public String getBenchmark() {
+        return benchmark;
+    }
+
+    public void setBenchmark(String benchmark) {
+        this.benchmark = benchmark;
+    }
+
+    public String[] getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(String[] nodes) {
+        this.nodes = nodes;
+    }
+
+    private String benchmark;
 
     public Integer getParallelism() {
         return parallelism;
