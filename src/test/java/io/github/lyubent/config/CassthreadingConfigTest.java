dd58ec6de1a6bb3f64bba097db271a9498925280
lyubent <lyuben@mail.com>
2018-12-16 01:17:29 +0000

    Add yaml test

---
+++ src/test/java/io/github/lyubent/config/CassthreadingConfigTest.java
@@ -0,0 +1,12 @@
+package io.github.lyubent.config;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CassthreadingConfigTest {
+    @Test
+    public void evaluatesExpression() {
+        CassthreadingConfig conf = CassthreadingConfig.getInstance();
+        assertEquals(conf.getParallelism(), new Integer(480));
+    }
+}
